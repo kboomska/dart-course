82cd92754fc31ac77db84a58719d9d54c007e418
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-21 11:46:56 +0300

    Lesson 1: Basics. const and final

--- lesson_1_basics/bin/lesson_1_basics.dart
+++ lesson_1_basics/bin/lesson_1_basics.dart
@@ -1,25 +1,6 @@
-late int tweets;
-
 void main() {
-  print('Hello Dart!');
+  const int myYearOfBirth = 1992;
+  // myYearOfBirth = 2003; // Error!
 
-  int number = 10;
-  // number = ''; // Error!
-
-  var numberOne = 10; // int
-
-  print(number);
-  number = 16;
-  print(number);
-
-  int like; // int like = null;
-  // print(like); // Error!
-  like = 5;
-  print(like);
-
-  tweets = 10;
-  print(tweets);
-
-  int? comment; // nullable
-  print(comment); // null
+  final hoursSinceMidnight = DateTime.now().hour;
 }
