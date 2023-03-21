a5427c892be1fbe29d3d54ac5edd34154c09d602
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-21 11:06:47 +0300

    Lesson 1: Basics. Variables

--- lesson_1_basics/bin/lesson_1_basics.dart
+++ lesson_1_basics/bin/lesson_1_basics.dart
@@ -1,1 +1,25 @@
-void main() {}
+late int tweets;
+
+void main() {
+  print('Hello Dart!');
+
+  int number = 10;
+  // number = ''; // Error!
+
+  var numberOne = 10; // int
+
+  print(number);
+  number = 16;
+  print(number);
+
+  int like; // int like = null;
+  // print(like); // Error!
+  like = 5;
+  print(like);
+
+  tweets = 10;
+  print(tweets);
+
+  int? comment; // nullable
+  print(comment); // null
+}
