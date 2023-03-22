adfe472be18f3c25c682d53e3d5ee1895c5e33f2
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-22 14:31:05 +0300

    Lesson 1: Basics. Object? and dynamic

--- lesson_1_basics/bin/lesson_1_basics.dart
+++ lesson_1_basics/bin/lesson_1_basics.dart
@@ -1,29 +1,19 @@
 void main() {
-  // Downcasting
-  num someNumber = 5;
-  final someInt = someNumber as int;
-  print(someInt.isEven); // false
+  var myVariable = 10;
+  // myVariable = 'hello'; // Error!
 
-  // final someDouble = someNumber as double;
-  // print(someDouble);  // Error: 5 isn't a double!
+  dynamic myDynamicVariable = 10;
+  myDynamicVariable = 'hello';
+  print(myDynamicVariable);
 
-  final someDouble = someNumber.toDouble();
-  print(someDouble); // 5.0
+  // We didn't know in advance what type
+  var myDynamic; // dynamic
+  myDynamic = 10;
+  myDynamic = 'hello';
 
-  // String -> int
-  var myInt = int.parse('10');
-  print(myInt); // 10
-
-  // String -> double
-  var myDouble = double.parse('5.5');
-  print(myDouble); // 5.5
-
-  // int -> String
-  String s1 = 10.toString();
-  String s2 = myInt.toString();
-
-  // double -> String
-  String s3 = 3.14159.toString();
-  String s4 = 3.14159.toStringAsFixed(2);
-  print(s4); // 3.14
+  // We knew exactly in advance that there would be
+  // a changeable type.
+  Object? myObjVariable;
+  myObjVariable = 10;
+  myObjVariable = 'hello';
 }
