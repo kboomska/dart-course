432c78f172b33e28595c5cb98f8f24a982ff57e0
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-22 16:01:35 +0300

    Lesson 2: Control Flow. if / else

---
+++ lesson_2_control_flow/bin/lesson_2_control_flow.dart
@@ -0,0 +1,41 @@
+/// *******************************************************************************************
+/// if / else
+
+void main() {
+  // if / else
+  var grade = 4;
+
+  if (grade == 5) {
+    print('Excellent grade of A');
+  } else {
+    print('Very good!');
+  }
+
+  // if else if
+  var marks = 60;
+
+  if (marks >= 90 && marks < 100) {
+    print('A+ grade');
+  } else if (marks >= 80 && marks < 90) {
+    print('A grade');
+  } else if (marks >= 60 && marks < 80) {
+    print('B grade');
+  } else if (marks >= 30 && marks < 60) {
+    print('C grade');
+  } else {
+    print('Invalid marks! Please try again!');
+  }
+
+  // ? :
+  int x = 1;
+  int y = 2;
+
+  // if (x < y) {
+  //   print(x + y);
+  // } else {
+  //   print(x - y);
+  // }
+
+  int z = x < y ? (x + y) : (x - y);
+  print(z); // 3
+}
