f71c7b7574dedd0bf4f503a560eafd81f75d5429
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-26 16:36:53 +0300

    Lesson 12: Null safety. Flow Analysis

--- lesson_12_null_safety/bin/lesson_12_null_safety.dart
+++ lesson_12_null_safety/bin/lesson_12_null_safety.dart
@@ -6,6 +6,7 @@
 // 2. Incrementally adoptable
 // 3. Fully sound
 
+/*
 void main() {
   int? age; // Nullable
   String name = 'Mark'; // Non-nullable
@@ -14,4 +15,33 @@ void main() {
   print(name); // Mark
   print(age); // null
   print(name1); // null
+}
+*/
+
+/// *******************************************************************************************
+/// Flow Analysis: Promotion & Definite Assignment
+
+void main() {
+  print(someValue(25)); // 25
+  print(someValue(null)); // 0
+
+  int x;
+  if (25 > 0) {
+    x = 1;
+  } else {
+    x = -1;
+  }
+  print(x);
+}
+
+int someValue(int? value) {
+  if (value == null) {
+    return 0;
+    // return valueIsNotDefined();
+  }
+  return value;
+}
+
+Never valueIsNotDefined() {
+  throw ArgumentError('Value is not defined');
 }
