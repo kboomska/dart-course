31f05122ec82430b9df7f167eb420e58ac61f1a4
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-26 14:32:36 +0300

    Lesson 11: Asynchronous programming. async / await

--- lesson_11_asynchronous_programming/bin/lesson_11_asynchronous_programming.dart
+++ lesson_11_asynchronous_programming/bin/lesson_11_asynchronous_programming.dart
@@ -1,6 +1,7 @@
 /// *******************************************************************************************
 /// Future
 
+/*
 import 'dart:html';
 
 void main() {
@@ -24,4 +25,38 @@ printFileContent() {
 
   // Instance of '_Future<String>'
   // print('The content on the file is --> $fileContent');
+}
+*/
+
+/// *******************************************************************************************
+/// async / await
+
+import 'dart:html';
+
+void main() {
+  print('The Main program: Starts');
+  printFileContent();
+  print('The Main program: Ends');
+}
+
+downloadFile() {
+  Future<String> result = Future.delayed(Duration(seconds: 5), () {
+    return HttpRequest.getString('https://dart.dev/robots.txt');
+  });
+  return result;
+}
+
+printFileContent() async {
+  try {
+//     String time = await HttpRequest.getString('https://www.deepl.com/robots.txt');
+//     print(time);
+
+    String fileContent = await downloadFile();
+    print('The content on the file is --> $fileContent');
+
+//     String time1 = await HttpRequest.getString('https://www.deepl.com/robots.txt');
+//     print(time1);
+  } catch (error) {
+    print('Catching error: $error');
+  }
 }
