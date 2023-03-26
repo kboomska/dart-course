993078f552192e73a68d58962257ed3c2023965f
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-26 11:56:06 +0300

    Lesson 10: Classes and Objects. Overriding methods, getters and setters

--- lesson_10_classes_and_objects/bin/lesson_10_classes_and_objects.dart
+++ lesson_10_classes_and_objects/bin/lesson_10_classes_and_objects.dart
@@ -57,6 +57,7 @@ class Moto extends Vehicle {
 /// *******************************************************************************************
 /// Inheritance and Constructors
 
+/*
 void main() {
   // var myCar = Car();
   // myCar.carName = 'BMW';
@@ -87,6 +88,61 @@ class Car extends Vehicle {
   Car(String carName, String color) : super.fromColor(color) {
     this.carName = carName;
     print('Call of Car. Name: $carName, Color: $color');
+  }
+
+  void open() {
+    print('Open doors');
+  }
+}
+*/
+
+/// *******************************************************************************************
+/// Overriding methods, getters and setters
+
+void main() {
+  var myCar = Car('BMW', 100, 'White');
+  myCar.start();
+}
+
+class Vehicle {
+  String? color;
+
+  Vehicle.fromColor(var color) {
+    this.color = color;
+    print('Call Vehicle.fromColor: $color Constructor');
+  }
+
+  void start() {
+    print('Has started');
+  }
+}
+
+class Car extends Vehicle {
+  String? carName;
+  int? speed;
+
+  Car(String carName, int speed, String color) : super.fromColor(color) {
+    this.carName = carName;
+    this.speed = speed;
+    print('Call of Car. Name: $carName, Color: $color');
+  }
+
+  String? get color => super.color;
+
+  void set color(String? value) {
+    if (value == 'Black') {
+      throw ArgumentError('Please, set other color');
+    }
+
+    super.color = color;
+  }
+
+  @override
+  void start() {
+    print('The $color $carName');
+    // print('Has started');
+    super.start(); // Has started
+    print('Speed: $speed');
   }
 
   void open() {
