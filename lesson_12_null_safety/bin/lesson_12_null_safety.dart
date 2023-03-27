52d7c7550e52d91f416fdb1109c6d8c59413adab
Aleksey Kuzmin <kboomska@gmail.com>
2023-03-27 11:42:12 +0300

    Lesson 12: Null safety. Required named parameters

--- lesson_12_null_safety/bin/lesson_12_null_safety.dart
+++ lesson_12_null_safety/bin/lesson_12_null_safety.dart
@@ -178,6 +178,7 @@ class Pizza {
 /// *******************************************************************************************
 /// Null safety and collections
 
+/*
 void main() {
   // Lists
   List<String?> nullableValues = [null, 'London', null, 'Moscow', 'NY'];
@@ -238,4 +239,47 @@ void main() {
   Map<String, int>? nullableMap;
   Map<String, int?> nullableValuesMap = {'null': null};
   Map<String, int?>? nullableMapAndNullableValues;
+}
+*/
+
+/// *******************************************************************************************
+/// Required named parameters
+
+// Positional mandatory = f(int x)
+// Positional optional  = f([int x])
+// Named optional       = f({int x})
+// Named mandatory      = ???
+
+void main() {
+  // var pizza = Pizza(1, 'Spring', 12);
+  // var pizza = Pizza();
+  // var pizza = Pizza(id: 1, name: 'Spring', price: 12);
+  // var pizza = Pizza(id: 1, name: null, price: 12);
+  var pizza = Pizza(name: 'Spring');
+
+  print(pizza.printPizza());
+}
+
+class Pizza {
+  late final int id;
+  final String name;
+  final double? price;
+
+  // Pizza(this.id, this.name, this.price);
+  // Pizza(this.id, this.name, [this.price]);
+  // Pizza({
+  //   required this.id,
+  //   required this.name,
+  //   required this.price,
+  // });
+  Pizza({
+    // this.id,
+    required this.name,
+    this.price,
+  }) : id = 1;
+
+  String printPizza() {
+    print('Coocking pizza...');
+    return 'Pizza ($id): $name, price: ${price ?? 'No price'}';
+  }
 }
